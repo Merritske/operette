5a85234e60c5db637da492b7f1fc6e6980fb73b2
merritske <merritske@gmail.com>
2023-06-20 22:36:58 +0200

    klaar?

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var ondermenu = document.getElementById("ondermenu")
+const ondermenu = document.getElementById("ondermenu")
+
 function show(){
     console.log(ondermenu)
 ondermenu.style.display = 'block'
-ondermenu.classList.add('selected')
+ondermenu.parentElement.classList.add('selected')
 console.log('klik')
 }
 function verberg(){
     ondermenu.classList.remove('selected')
     ondermenu.style.display ='none'
 }
+
+// const menu = document.getElementById('menu-lijst')
+// menu.addEventListener('click', console.log(menu))
+// function setActive(){
+// console.log(menu)
+
+// }
+document.querySelectorAll(".nav-link").forEach((link) => {
+    console.log(link.innerHTML)
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+      console.log(link.href)
+    }
+});
